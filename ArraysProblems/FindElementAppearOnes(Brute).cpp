5be6b1e9d6cdf6110f002ae3abcc8c1b12b46eea
Abhinav <psaiabhinav1724@gmail.com>
2025-08-06 18:17:16 +0530

    Arrays - Find Element Appear Ones

---
+++ ArraysProblems/FindElementAppearOnes(Brute).cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int XOR=0;
+    for(int i=0;i<n;i++){
+        XOR^=arr[i];
+    }
+    cout<<XOR;
+
+    return 0;
+}
