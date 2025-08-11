6f0ea9291c00a3d50595269449fc9be4f756bf39
Abhinav <psaiabhinav1724@gmail.com>
2025-08-11 19:50:20 +0530

    Selection Sort

--- Sorting/Selection.cpp
+++ Sorting/Selection.cpp
@@ -1,26 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-void selection_sort(vector<int>& arr,int n){
-      for(int i=0;i<=n-2;i++){
-        int min=i;
+void selection_sort(vector<int> &arr,int n){
+    for(int i=0;i<=n-2;i++){
+       int  mini=i;
         for(int j=i;j<=n-1;j++){
-            if(arr[j]<arr[min]){
-                min=j;
+            if(arr[j]<arr[mini]){
+                mini=j;
             }
         }
-        swap(arr[i],arr[min]);
-      }
+        swap(arr[mini],arr[i]);
+    }
 }
 int main(){
     int n;
+    cout<<"Enter the size:";
     cin>>n;
     vector<int> arr(n);
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
     selection_sort(arr,n);
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+    for(auto it:arr){
+        cout<<it<<" , ";
     }
     return 0;
 }
